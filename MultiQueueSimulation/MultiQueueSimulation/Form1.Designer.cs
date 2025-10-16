cd7544632c2fe996b781d8ddc168faeb033739b1
Omar0903 <3mr2brahim932004@gmail.com>
2025-10-16 23:52:19 +0300

    input from gui and logic done

---
+++ MultiQueueSimulation/MultiQueueSimulation/Form1.Designer.cs
@@ -0,0 +1,244 @@
+﻿using System.Windows.Forms;
+
+namespace MultiQueueSimulation
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.label5 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.btnRunSimulation = new System.Windows.Forms.Button();
+            this.dataGridViewResults = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewResults)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(651, 203);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(45, 26);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(77, 201);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(238, 28);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Stopping Condition ";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(336, 275);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(309, 28);
+            this.comboBox1.TabIndex = 4;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // label2
+            // 
+            this.label2.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(77, 275);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(238, 28);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Server Selection method";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // label3
+            // 
+            this.label3.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(77, 140);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(238, 29);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Number of servers";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(336, 143);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(145, 26);
+            this.textBox2.TabIndex = 7;
+            this.textBox2.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Location = new System.Drawing.Point(336, 201);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(309, 28);
+            this.comboBox2.TabIndex = 8;
+            this.comboBox2.SelectedIndexChanged += new System.EventHandler(this.comboBox2_SelectedIndexChanged);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 26F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(490, 28);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(657, 59);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Multi server - single queue";
+            this.label4.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.BackgroundColor = System.Drawing.SystemColors.ControlLightLight;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(47, 549);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 62;
+            this.dataGridView1.RowTemplate.Height = 28;
+            this.dataGridView1.Size = new System.Drawing.Size(361, 228);
+            this.dataGridView1.TabIndex = 10;
+            // 
+            // label5
+            // 
+            this.label5.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(77, 328);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(238, 27);
+            this.label5.TabIndex = 11;
+            this.label5.Text = "Entre numbers of rows";
+            this.label5.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.label5.Click += new System.EventHandler(this.label5_Click);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(336, 329);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(231, 26);
+            this.textBox3.TabIndex = 12;
+            this.textBox3.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.textBox3.TextChanged += new System.EventHandler(this.textBox3_TextChanged);
+            // 
+            // panel1
+            // 
+            this.panel1.AutoScroll = true;
+            this.panel1.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.panel1.Location = new System.Drawing.Point(432, 511);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1247, 302);
+            this.panel1.TabIndex = 13;
+            // 
+            // btnRunSimulation
+            // 
+            this.btnRunSimulation.Location = new System.Drawing.Point(1522, 856);
+            this.btnRunSimulation.Name = "btnRunSimulation";
+            this.btnRunSimulation.Size = new System.Drawing.Size(137, 32);
+            this.btnRunSimulation.TabIndex = 14;
+            this.btnRunSimulation.Text = "Run Simulation";
+            this.btnRunSimulation.UseVisualStyleBackColor = true;
+            this.btnRunSimulation.Click += new System.EventHandler(this.btnRunSimulation_Click);
+            // 
+            // dataGridViewResults
+            // 
+            this.dataGridViewResults.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridViewResults.Location = new System.Drawing.Point(47, 877);
+            this.dataGridViewResults.Name = "dataGridViewResults";
+            this.dataGridViewResults.RowHeadersWidth = 62;
+            this.dataGridViewResults.RowTemplate.Height = 28;
+            this.dataGridViewResults.Size = new System.Drawing.Size(1193, 295);
+            this.dataGridViewResults.TabIndex = 15;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.ClientSize = new System.Drawing.Size(1910, 1209);
+            this.Controls.Add(this.dataGridViewResults);
+            this.Controls.Add(this.btnRunSimulation);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.Name = "Form1";
+            this.Text = "Modeling and Simulation Task";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewResults)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private Label label2;
+        private Label label3;
+        private TextBox textBox2;
+        private ComboBox comboBox2;
+        private Label label4;
+        private DataGridView dataGridView1;
+        private Label label5;
+        private TextBox textBox3;
+        private Panel panel1;
+        private Button btnRunSimulation;
+        private DataGridView dataGridViewResults;
+    }
+}
+
---
+++ MultiQueueSimulation/MultiQueueSimulation/Form1.cs
@@ -0,0 +1,268 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MultiQueueModels;
+using MultiQueueTesting;
+
+namespace MultiQueueSimulation
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            comboBox1.Items.Add("Priority");
+            comboBox1.Items.Add("Random");
+            comboBox1.Items.Add("Least utilization");
+
+            comboBox1.DropDownStyle = ComboBoxStyle.DropDownList;
+            comboBox1.SelectedIndex = 0;
+            comboBox2.Items.Add("Maximum Number of customers");
+            comboBox2.Items.Add("Simulation end time");
+            comboBox2.DropDownStyle = ComboBoxStyle.DropDownList;
+            comboBox2.SelectedIndex = 0;
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+        }
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // جدول الـ Interarrival
+            dataGridView1.Columns.Add("InterarrivalTime", "Interarrival Time");
+            dataGridView1.Columns.Add("Probability", "Probability");
+            InitializeMainTable(5);
+            dataGridView1.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
+            dataGridView1.AllowUserToAddRows = false;
+            dataGridView1.AllowUserToDeleteRows = false;
+
+            panel1.AutoScroll = true;
+
+            // جدول النتائج
+            dataGridViewResults.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
+            dataGridViewResults.AllowUserToAddRows = false;
+            dataGridViewResults.AllowUserToDeleteRows = false;
+
+            // إنشاء الأعمدة المطلوبة لعرض كل القيم
+            dataGridViewResults.Columns.Add("CustomerNo", "Customer No");
+            dataGridViewResults.Columns.Add("RandInterArrival", "Random Digit for Inter-Arrival");
+            dataGridViewResults.Columns.Add("InterArrival", "Inter-Arrival Time");
+            dataGridViewResults.Columns.Add("ArrivalTime", "Arrival Time (Clock Time)");
+            dataGridViewResults.Columns.Add("RandService", "Random Digit for Service Duration");
+            dataGridViewResults.Columns.Add("ServiceTime", "Service Duration");
+            dataGridViewResults.Columns.Add("ServerIndex", "Server Index");
+            dataGridViewResults.Columns.Add("StartTime", "Time Service Begins");
+            dataGridViewResults.Columns.Add("EndTime", "Time Service Ends (Departure)");
+            dataGridViewResults.Columns.Add("TimeInQueue", "Total Delay (Time in Queue)");
+        }
+
+
+        private void InitializeMainTable(int numRows)
+        {
+            dataGridView1.Rows.Clear();
+            for (int i = 0; i < numRows; i++)
+                dataGridView1.Rows.Add();
+        }
+        private void label5_Click(object sender, EventArgs e)
+        {
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+        }
+
+        private void comboBox2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+            UpdateSubTables();
+
+        }
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+            int numRows;
+            if (int.TryParse(textBox3.Text, out numRows) && numRows > 0)
+            {
+                InitializeMainTable(numRows);
+            }
+            else
+            {
+                numRows = 5;
+                InitializeMainTable(numRows);
+            }
+            UpdateSubTables();
+        }
+
+        private void UpdateSubTables()
+        {
+            int numTables;
+            int numRows = dataGridView1.Rows.Count > 0 ? dataGridView1.Rows.Count : 5;
+
+            if (int.TryParse(textBox2.Text, out numTables) && numTables > 0)
+            {
+                panel1.Controls.Clear();
+
+                int xOffset = 10;
+                int yOffset = 10;
+
+                for (int i = 0; i < numTables; i++)
+                {
+                    Label lbl = new Label();
+                    lbl.Text = $"Server {i + 1}";
+                    lbl.Font = new Font("Segoe UI", 10, FontStyle.Bold);
+                    lbl.AutoSize = true;
+                    lbl.Left = xOffset + 30;
+                    lbl.Top = yOffset;
+
+                    Panel subPanel = new Panel();
+                    subPanel.Left = xOffset;
+                    subPanel.Top = lbl.Bottom + 5;
+                    subPanel.BackColor = Color.White;
+                    subPanel.BorderStyle = BorderStyle.FixedSingle;
+
+                    DataGridView dgv = new DataGridView();
+                    dgv.Name = $"serverTable{i + 1}";
+                    dgv.Width = dataGridView1.Width;
+                    dgv.AutoSizeColumnsMode = dataGridView1.AutoSizeColumnsMode;
+                    dgv.AllowUserToAddRows = false;
+                    dgv.AllowUserToDeleteRows = false;
+                    dgv.RowHeadersVisible = false;
+
+                    dgv.Columns.Add("ServiceTime", "Service Time");
+                    dgv.Columns.Add("Probability", "Probability");
+
+                    for (int r = 0; r < numRows; r++)
+                        dgv.Rows.Add();
+
+                    int headerHeight = dgv.ColumnHeadersHeight;
+                    int rowsHeight = dgv.RowTemplate.Height * numRows;
+                    dgv.Height = headerHeight + rowsHeight + 2;
+
+                    subPanel.Width = dgv.Width + 2;
+                    subPanel.Height = dgv.Height + 2;
+                    subPanel.Controls.Add(dgv);
+
+                    panel1.Controls.Add(lbl);
+                    panel1.Controls.Add(subPanel);
+
+                    xOffset += subPanel.Width + 20;
+                }
+            }
+            else
+            {
+                panel1.Controls.Clear();
+            }
+        }
+
+        private void btnRunSimulation_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                SimulationSystem system = new SimulationSystem();
+
+                // اختيار طريقة اختيار السيرفر
+                string selectedMethod = comboBox1.SelectedItem.ToString();
+                if (selectedMethod == "Random")
+                    system.SelectionMethod = Enums.SelectionMethod.Random;
+                else if (selectedMethod == "Priority")
+                    system.SelectionMethod = Enums.SelectionMethod.HighestPriority;
+                else
+                    system.SelectionMethod = Enums.SelectionMethod.LeastUtilization;
+
+                // قراءة جدول الـ Interarrival
+                foreach (DataGridViewRow row in dataGridView1.Rows)
+                {
+                    if (row.Cells[0].Value != null && row.Cells[1].Value != null)
+                    {
+                        TimeDistribution td = new TimeDistribution
+                        {
+                            Time = int.Parse(row.Cells[0].Value.ToString()),
+                            Probability = decimal.Parse(row.Cells[1].Value.ToString())
+                        };
+                        system.InterarrivalDistribution.Add(td);
+                    }
+                }
+
+                // قراءة عدد السيرفرات والعملاء
+                int numServers = int.Parse(textBox2.Text);
+                system.NumberOfServers = numServers;
+                system.StoppingNumber = int.Parse(textBox1.Text);
+                system.StoppingCriteria = Enums.StoppingCriteria.NumberOfCustomers;
+
+                // قراءة كل سيرفر
+                for (int i = 0; i < numServers; i++)
+                {
+                    DataGridView dgv = panel1.Controls.Find($"serverTable{i + 1}", true).FirstOrDefault() as DataGridView;
+                    if (dgv != null)
+                    {
+                        Server server = new Server();
+                        server.ID = i + 1;
+                        foreach (DataGridViewRow r in dgv.Rows)
+                        {
+                            if (r.Cells[0].Value != null && r.Cells[1].Value != null)
+                            {
+                                server.TimeDistribution.Add(new TimeDistribution
+                                {
+                                    Time = int.Parse(r.Cells[0].Value.ToString()),
+                                    Probability = decimal.Parse(r.Cells[1].Value.ToString())
+                                });
+                            }
+                        }
+                        system.Servers.Add(server);
+                    }
+                }
+
+                // تشغيل المحاكاة
+                system.RunSimulation();
+
+                // عرض النتائج في الجدول
+                dataGridViewResults.Rows.Clear();
+                foreach (var c in system.SimulationTable)
+                {
+                    dataGridViewResults.Rows.Add(
+                        c.CustomerNumber,
+                        c.RandomInterArrival,
+                        c.InterArrival,
+                        c.ArrivalTime,
+                        c.RandomService,
+                        c.ServiceTime,
+                        c.AssignedServer.ID,
+                        c.StartTime,
+                        c.EndTime,
+                        c.TimeInQueue
+                    );
+                }
+
+                // عرض الملخص
+                string summary = $"Average Waiting Time: {system.PerformanceMeasures.AverageWaitingTime}\n" +
+                                 $"Waiting Probability: {system.PerformanceMeasures.WaitingProbability}\n" +
+                                 $"Max Queue Length: {system.PerformanceMeasures.MaxQueueLength}";
+                MessageBox.Show(summary, "Simulation Results");
+                string testingResult = TestingManager.Test(system, Constants.FileNames.TestCase1);
+                MessageBox.Show(testingResult);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error: " + ex.Message);
+            }
+        }
+    }
+}
